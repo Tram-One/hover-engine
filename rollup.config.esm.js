34c53a38dcd926eb176a07112f767e0997b0d99b
Jesse Jurman <j.r.jurman@gmail.com>
2018-05-07 01:33:20 -0400

    Package cleanup (#18)

--- rollup.config.esm.js
+++ rollup.config.esm.js
@@ -2,8 +2,6 @@ const buble = require('rollup-plugin-buble')
 const filesize = require('rollup-plugin-filesize')
 const pkg = require('./package.json')
 
-const external = Object.keys(pkg.dependencies)
-
 const plugins = [
   buble({target: {node: 6}}),
   filesize()
@@ -11,7 +9,6 @@ const plugins = [
 
 export default {
   input: 'hover-engine.js',
-  external: external,
   plugins: plugins,
   output: {
     format: 'es',
