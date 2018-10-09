9181123f99470702d74b11a2df8fabcd3ab5ba65
jjurman <jjurman@constantcontact.com>
2018-10-09 18:12:25 -0400

    fix: remove testing bug

--- examples/counter.js
+++ examples/counter.js
@@ -12,7 +12,7 @@ engine.addListener((store, actions, actionName, actionArguments) => {
   console.log(actionName, actionArguments, '->', store)
 })
 
-engine.actions.add(5, 20) // add 5 -> { num: 5 }
+engine.actions.add(5) // add 5 -> { num: 5 }
 engine.actions.add(8) // add 8 -> { num: 13 }
 engine.actions.sub(5) // sub 5 -> { num: 8 }
 console.log(engine.store) // { num: 8 }
